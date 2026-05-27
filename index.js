cc1a960976255143ac6119298852719e78a49e0c
Nicole Gardner <windatheart261392@gmail.com>
2026-05-26 22:17:51 -0500

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-async function fetchMovies() {
-  const res = await fetch("https://www.omdbapi.com/?apikey=51106b2b&s=harry%20potter");
-  const data = await res.json();
+async function fetchMovies(searchValue = "harry potter") {
+  const res = await fetch(
+    `https://www.omdbapi.com/?apikey=51106b2b&s=${searchValue}`
+  );
 
-data.Search.forEach(movie => {
-  const movieContainer = document.querySelector(".movie__container");
-  const movieElement = document.createElement("div");
-  movieElement.classList.add("movie");
-  movieElement.innerHTML = `
-    <img class="movie__poster" src="${movie.Poster}" alt="${movie.Title}">
-    <div class="movie__info">
-      <h2>${movie.Title}</h2>
-      <p>Release Year: ${movie.Year}</p>
-      <p>Genre: Fantasy</p>
-    </div>
-  `;
-  movieContainer.appendChild(movieElement);
-});
+  const data = await res.json();
+  console.log(data);
 }
-fetchMovies();
+const searchInput = document.querySelector(".search__input");
+
+searchInput.addEventListener("input", function (event) {
+  const searchValue = event.target.value;
+  fetchMovies(searchValue);
+});
