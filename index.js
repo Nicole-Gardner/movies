352d51c7dc4c458437b44ce7f6d42bd905a1fc95
Nicole Gardner <windatheart261392@gmail.com>
2026-05-26 21:20:35 -0500

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-    const movies = [
-  {
-    Title: "Harry Potter and the Deathly Hallows: Part 2",
-    Year: "2011",
-    Poster: "..."
-  },
-  {
-    Title: "Harry Potter and the Sorcerer's Stone",
-    Year: "2001",
-    Poster: "https://m.media-amazon.com/images/M/MV5BNTU1MzgyMDMtMzBlZS00YzczLThmYWEtMjU3YmFlOWEyMjE1XkEyXkFqcGc@._V1_SX300.jpg"
-  }
-];
+async function fetchMovies() {
+  const res = await fetch("https://www.omdbapi.com/?apikey=51106b2b&s=harry");
+  const data = await res.json();
 
-function renderMovies(movieList) {
-  const moviesWrapper = document.querySelector(" .movie__container");
-
-  moviesWrapper.innerHTML = movieList
-    .map(movie => `
-      <div class="movie">
-        <img class="movie__poster" src="${movie.Poster}" alt="${movie.Title}">
-        <div class="movie__info">
-          <h2>${movie.Title}</h2>
-          <p>Release Year: ${movie.Year}</p>
-          <p>Genre: Fantasy</p>
-        </div>
-      </div>
-    `)
-    .join("");
+data.Search.forEach(movie => {
+  const movieContainer = document.querySelector(".movie__container");
+  const movieElement = document.createElement("div");
+  movieElement.classList.add("movie");
+  movieElement.innerHTML = `
+    <img class="movie__poster" src="${movie.Poster}" alt="${movie.Title}">
+    <div class="movie__info">
+      <h2>${movie.Title}</h2>
+      <p>Release Year: ${movie.Year}</p>
+      <p>Genre: Fantasy</p>
+    </div>
+  `;
+  movieContainer.appendChild(movieElement);
+});
 }
-
-function sortOldestToNewest() {
-  movies.sort((a, b) => Number(a.Year) - Number(b.Year));
-  renderMovies(movies);
-}
-
-renderMovies(movies);
+fetchMovies();
