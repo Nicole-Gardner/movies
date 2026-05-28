afae287251691b7eaa7f699574c28dffa1e4eb4c
Nicole Gardner <windatheart261392@gmail.com>
2026-05-27 21:39:17 -0500

    first commmit

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const sortFilter = document.querySelector("#sortFilter");
 const searchInput = document.querySelector("#searchInput");
-const sortFilter = document.querySelector("#sortFilter");
-const moviecontainer = document.querySelector(".movie__container");
+const sortFilter = document.querySelector("#sortFilter"); // Only declare this once
+const movieContainer = document.querySelector(".movie__container"); // Fixed variable name
 
 async function fetchMovies(searchValue = "harry potter") {
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=51106b2b&s=${searchValue}`
-  );
-
+  const res = await fetch(`https://www.omdbapi.com/?apikey=51106b2b&s=${searchValue}`);
   const data = await res.json();
   console.log(data);
 
+
   movieContainer.innerHTML = "";
 
+ 
   if (!data.Search) {
     movieContainer.innerHTML = "<p>No movies found.</p>";
     return;
   }
-  let movies = data.Search;
-if (sortFilter.value === "newest") {
-  movies.sort((a, b) => b.Year - a.Year);
-}
-if (sortFilter.value === "oldest") {
-  movies.sort((a, b) => a.Year - b.Year);
-}
-movies.forEach((movie) => {
 
-  data.Search.forEach((movie) => {
+  let movies = data.Search;
+
+
+  if (sortFilter.value === "newest") {
+    movies.sort((a, b) => b.Year - a.Year);
+  } else if (sortFilter.value === "oldest") {
+    movies.sort((a, b) => a.Year - b.Year);
+  }
+
+  movies.forEach((movie) => {
     movieContainer.innerHTML += `
       <div class="movie">
         <img class="movie__poster" src="${movie.Poster}" alt="${movie.Title}">
@@ -42,6 +41,10 @@ movies.forEach((movie) => {
 searchInput.addEventListener("input", function (event) {
   const searchValue = event.target.value;
   fetchMovies(searchValue);
-});
+}
+);
 
-fetchMovies();
+sortFilter.addEventListener("change", function () {
+  const searchValue = searchInput.value || "harry potter";
+  fetchMovies(searchValue);
+});
